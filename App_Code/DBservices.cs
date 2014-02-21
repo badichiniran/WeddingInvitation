aa55070d2a229f21170936e984c29f1b4595bd48
badichiniran <badichiniran@gmail.com>
2014-02-21 23:51:13 +0200

    add dbservices

---
+++ App_Code/DBservices.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+/// <summary>
+/// Summary description for DBservices
+/// </summary>
+public class DBservices
+{
+        public SqlDataAdapter da;
+        public DataTable dt;
+        public string connectionString;
+        public DBservices()
+        {
+            connectionString = WebConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+        }
+
+        public SqlConnection connect()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                con.Open();
+                return con;
+            }
+            catch (Exception ex)
+            {
+                //Logger.writeToLog(LoggerLevel.ERROR, "page :DBServicesAPP.cs, function: connect(), exeption message: " + ex.Message);
+                throw;
+            }
+        }
+
+        public void disconnect(SqlConnection con)
+        {
+            if (con != null)
+            {
+                try
+                {
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                   // Logger.writeToLog(LoggerLevel.ERROR, "page :DBServicesAPP.cs, function: disconnect(), exeption message: " + ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        public void FinishShopping(string name, string amount, string comment)
+        {
+            string InsertGuestQuery = "INSERT INTO [GuestList] ([Name],[Amount],[Comment]) VALUES ('" + name + "','" + amount + "','" + comment + "')";
+            SqlConnection con;
+            try
+            {
+                con = connect();
+                da = new SqlDataAdapter(InsertGuestQuery, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            disconnect(con);
+        }
+
+}
---
+++ InsertGuest.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class InsertGuest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DBservices dbs = new DBservices();
+        if (Request.QueryString == null)
+        {
+            Response.End();
+            return;
+        }
+        NameValueCollection requestQuery = Request.QueryString;
+
+        try
+        {
+            string name = requestQuery["name"];
+            string amount = requestQuery["amount"];
+            //string comment = requestQuery["comment"];
+            dbs.FinishShopping(name,amount,"NULL");
+
+        }
+
+        catch (Exception ex)
+        {
+           // Logger.writeToLog(LoggerLevel.ERROR, "page :ActivityShow.aspx.cs, the exeption message is : " + ex.Message);
+
+        }
+
+
+
+        Response.End();
+
+    }
+
+
+}
